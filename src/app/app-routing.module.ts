336f45e33b11d9472f514d2b2a6256d83129e4ef
Francisco Roca <frrocaguilar@gmail.com>
2020-10-19 21:37:23 +0200

    token list initial implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  {
+    path: '',
+    children: [
+      { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+    ]
+  }
 ];
 
 @NgModule({
